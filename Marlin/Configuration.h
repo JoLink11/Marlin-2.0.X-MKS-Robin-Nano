b313a461df7c0f9d4cf0fc608cd1d7e6cbd3d1a5
JoLink11 <46500603+JoLink11@users.noreply.github.com>
2020-06-23 02:26:18 +0200

    Author mod added

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -247,7 +247,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(le3tspeak, MKS Robin Nano)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(le3tspeak mod by JL24, MKS Robin Nano)" // Who made the changes.
 #define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
